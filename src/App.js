fcb56772e14bf2312a6a4031298c3dc65927198e
M Haidar Hanif <git@mhaidarhanif.com>
2020-07-24 23:11:27 +0700

    Start making simple page

--- src/App.js
+++ src/App.js
@@ -1,26 +1,15 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>High Skill Masters</h1>
+      <p>
+        High Skill Masters is a global community for highly skilled
+        professionals in several countries that aimed to be connected worldwide.
+      </p>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
